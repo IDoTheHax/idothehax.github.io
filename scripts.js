57757b4045dd8ed7b78e898817461c8f785596dd
IDoTheHax <hax@idothehax.com>
2024-06-21 09:44:21 +0200

    - FIxxed the toggle but didnt set it dark by default

--- scripts.js
+++ scripts.js
@@ -1,23 +1,20 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const toggle = document.getElementById('darkModeToggle');
-    const currentTheme = localStorage.getItem('theme');
-    
-    if (currentTheme) {
-        document.body.classList.add(currentTheme);
-        if (currentTheme === 'dark-mode') {
-            toggle.checked = true;
-        }
-    }
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const body = document.body;
 
-    toggle.addEventListener('change', () => {
-        if (toggle.checked) {
-            document.body.classList.add('dark-mode');
-            document.body.classList.remove('light-mode');
-            localStorage.setItem('theme', 'dark-mode');
+    // Dark mode toggle
+    darkModeToggle.addEventListener('change', () => {
+        body.classList.toggle('dark-mode');
+        body.classList.toggle('light-mode');
+    });
+
+    
+    // Shrinking hax section on scroll
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            body.classList.add('shrink');
         } else {
-            document.body.classList.add('light-mode');
-            document.body.classList.remove('dark-mode');
-            localStorage.setItem('theme', 'light-mode');
+            body.classList.remove('shrink');
         }
     });
 });
